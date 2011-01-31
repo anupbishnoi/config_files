6a3f8fe5cf8d1e7ebdfdabe9cc54335be4f90222
Anup Bishnoi <pixelsallover@gmail.com>
2011-01-31 04:59:14 -0500

    added firstboot.sh and changed README

---
+++ firstboot.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+cd ~
+
+sudo chown -R $USER /usr/local
+mkdir ~/local
+echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
+
+git config --global user.name "Anup Bishnoi"
+git config --global user.email pixelsallover@gmail.com
+cd config_files
+git remote add origin git@github.com:anupbishnoi/config_files.git
+git commit -m "committing from fresh ubuntu install"
+git push origin master
+cd ~
+
+sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get -y install vim adobe-flashplugin build-essential rar unrar p7zip p7zip-full gstreamer0.10-pitfdll gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-bad-multiverse gstreamer0.10-plugins-ugly gstreamer0.10-plugins-ugly-multiverse vlc-nox libssl-dev chromium-browser ack-grep
+
+git clone git://github.com/ry/node.git
+cd node
+./configure --prefix=~/local
+make install 
+make doc
+cd ..
+
+git clone git://github.com/isaacs/npm.git
+cd npm
+make install
+
+cat >>~/.npmrc <<NPMRC
+root = ~/.node_libraries
+binroot = ~/bin
+manroot = ~/share/man
+NPMRC
+
+
