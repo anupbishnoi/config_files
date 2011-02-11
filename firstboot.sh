03585ca090e539fc0828ab447ed9e0155e2223bd
Anup Bishnoi <pixelsallover@gmail.com>
2011-02-11 17:21:16 +0530

    renamed install.sh to setconfig.sh and added TODO

--- firstboot.sh
+++ firstboot.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 cd ~
-
+rmdir Public/ Pictures/ Videos/ Documents/ Music/ Templates/
 sudo chown -R $USER /usr/local
 mkdir ~/local
 
 sudo add-apt-repository ppa:passy/vim
+sudo add-apt-repository ppa:couchapp/couchapp
 sudo apt-get update
 
-sudo apt-get -y install curl zsh vim ctags vim-doc vim-scripts vim-gnome flashplugin-nonfree build-essential rar unrar p7zip p7zip-full gstreamer0.10-pitfdll gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-bad-multiverse gstreamer0.10-plugins-ugly gstreamer0.10-plugins-ugly-multiverse vlc-nox libssl-dev chromium-browser ack-grep xchm
+sudo apt-get -y install curl zsh vim ctags vim-doc vim-scripts vim-gnome flashplugin-nonfree build-essential rar unrar p7zip p7zip-full gstreamer0.10-pitfdll gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-bad-multiverse gstreamer0.10-plugins-ugly gstreamer0.10-plugins-ugly-multiverse vlc-nox libssl-dev chromium-browser ack-grep xchm bum rcconf
 
 sudo apt-get upgrade
 
+mkdir src
+cd src
 git clone git://github.com/ry/node.git
 cd node
 ./configure --prefix=~/local
@@ -22,11 +25,10 @@ cd ..
 git clone git://github.com/isaacs/npm.git
 cd npm
 make install
+cd ~
 
-cat >>~/.npmrc <<NPMRC
-root = ~/.node_libraries
-binroot = ~/bin
-manroot = ~/share/man
-NPMRC
+sudo apt-get install couchdb couchapp
 
-
+mkdir code
+cd code
+git clone git@github.com:anupbishnoi/informationgraph.git
