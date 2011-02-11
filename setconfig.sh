f94e4153e64fe55743640d2df69115a01605e83a
Anup Bishnoi <pixelsallover@gmail.com>
2011-02-11 17:22:19 +0530

    ok. TODO and setconfig got added only now

---
+++ setconfig.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cutstring="DO NOT EDIT BELOW THIS LINE"
+
+for name in *; do
+    target="$HOME/.$name"
+    if [ -e $target ]; then
+        if [ ! -L $target ]; then
+            cutline=`grep -n -m1 "$cutstring" "$target" | sed "s/:.*//"`
+            if [[ -n $cutline ]]; then
+                let "cutline = $cutline - 1"
+                echo "Updating $target"
+                head -n $cutline "$target" > update_tmp
+                startline=`tail -r "$name" | grep -n -m1 "$cutstring" | sed "s/:.*//"`
+                if [[ -n $startline ]]; then
+                    tail -n $startline "$name" >> update_tmp
+                else
+                    cat "$name" >> update_tmp
+                fi
+                mv update_tmp "$target"
+            else
+                echo "WARNING: $target exists but is not a symlink."
+            fi
+        fi
+    else
+        if [[ $name != 'install.sh' ]]; then
+            if [[ $name != 'firstboot.sh' ]]; then
+                echo "Creating $target"
+                if [[ -n `grep "$cutstring" "$name"` ]]; then
+                    cp "$PWD/$name" "$target"
+                else
+                    ln -s "$PWD/$name" "$target"
+                fi
+            fi
+        fi
+    fi
+done
+
+Pcutstring="PERSONAL"
+
+#git config --global user.name "Anup Bishnoi"
+#git config --global user.email pixelsallover@gmail.com
+#git remote add github git@github.com:anupbishnoi/config_files.git
