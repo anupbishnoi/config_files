92db5f378430fc36ffba0dd2e77469503f9fb567
Anup Bishnoi <pixelsallover@gmail.com>
2011-03-05 23:33:42 +0530

    added echo statements

--- done.sh
+++ done.sh
@@ -2,10 +2,12 @@
 
 cd ~/config_files
 git commit -a
+echo "pushing changes to config_files"
 git push
 
 cd ~/code/informationgraph
 git commit -a
+echo "pushing changes to informationgraph"
 git push
 
 cd ~
--- letscode.sh
+++ letscode.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 cd ~/config_files
+echo "pulling changes for config_files"
 git pull
 
 cd ~/code/informationgraph
+echo "pulling changes for informationgraph"
 git pull
 
 cd ~
