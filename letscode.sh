0b8a5ce01b7400f69ba9dbcd0ad29948123b6418
Anup Bishnoi <pixelsallover@gmail.com>
2011-04-01 20:30:36 +0530

    removed watching coffee files from letscode.sh

--- letscode.sh
+++ letscode.sh
@@ -8,8 +8,4 @@ cd ~/code/informationgraph
 echo "pulling changes for informationgraph"
 git pull
 
-coffee --watch --compile ~/code/informationgraph/home/_attachments/scripts/*.coffee &
-coffee --watch --compile ~/code/informationgraph/home/vendor/ig/_attachments/*.coffee &
-echo "listening to coffeescript changes for ig and igapp"
-
 cd ~
