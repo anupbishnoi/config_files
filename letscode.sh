ec5dc74c05a1a4617665abb087d659f3a16ad9a4
Anup Bishnoi <pixelsallover@gmail.com>
2011-04-02 13:19:25 +0530

    letscode.sh now commits local changes before pulling from github

--- letscode.sh
+++ letscode.sh
@@ -2,10 +2,12 @@
 
 cd ~/config_files
 echo "pulling changes for config_files"
+git commit -a
 git pull
 
 cd ~/code/informationgraph
 echo "pulling changes for informationgraph"
+git commit -a
 git pull
 
 cd ~
