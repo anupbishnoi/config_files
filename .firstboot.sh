469c2a4e3bec08b658ad0fe8a25d5e8a19da9f2e
Anup Bishnoi <pixelsallover@gmail.com>
2011-04-25 15:01:04 +0530

    added docco, for literate programming in coffeescript

--- .firstboot.sh
+++ .firstboot.sh
@@ -7,9 +7,12 @@ mkdir ~/local
 
 sudo add-apt-repository ppa:passy/vim
 sudo add-apt-repository ppa:couchapp/couchapp
+sudo add-apt-repository ppa:mozillateam/firefox-stable
 sudo apt-get update
 
-sudo apt-get -y install curl zsh vim ctags vim-doc vim-scripts vim-gnome flashplugin-nonfree build-essential rar unrar p7zip p7zip-full gstreamer0.10-pitfdll gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-bad-multiverse gstreamer0.10-plugins-ugly gstreamer0.10-plugins-ugly-multiverse vlc-nox libssl-dev chromium-browser ack-grep xchm bum rcconf rake ruby-dev lynx-cur irssi rlwrap
+sudo apt-get -y install curl zsh vim ctags vim-doc vim-scripts vim-gnome flashplugin-nonfree build-essential rar unrar p7zip p7zip-full gstreamer0.10-pitfdll gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-bad-multiverse gstreamer0.10-plugins-ugly gstreamer0.10-plugins-ugly-multiverse vlc-nox libssl-dev chromium-browser ack-grep xchm bum rcconf rake ruby-dev lynx-cur irssi rlwrap python-setuptools
+
+easy_install Pygments
 
 sudo apt-get upgrade
 
@@ -27,7 +30,7 @@ cd npm
 make install
 cd ~
 
-npm install connect express jade step less supervisor mingy optimist npmdep http-console less coffee-script
+npm install connect express jade step less supervisor mingy optimist npmdep http-console less coffee-script docco
 
 sudo apt-get install couchdb couchapp
 
